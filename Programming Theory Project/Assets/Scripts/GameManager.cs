5ab840787568625ab65e5a7b30ffce90a712bd56
GsSe7en <latyshev.fedor@gmail.com>
2021-07-30 15:58:52 +0300

    save name

--- Programming Theory Project/Assets/Scripts/GameManager.cs
+++ Programming Theory Project/Assets/Scripts/GameManager.cs
@@ -1,23 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance;
+    public string inputName;
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+    [System.Serializable] public class SaveData
+    {
+        public string name;
+    }
     public void StartGame()
     {
         SceneManager.LoadScene(1);
     }
-    // Start is called before the first frame update
-    void Start()
+    public void Exit()
     {
-        
+        #if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+        #else
+        Application.Quit();
+        #endif
     }
-
-    // Update is called once per frame
-    void Update()
+    public void SaveName(string name)
     {
-        
+        inputName = name;
+    }
+    public class Enemy : MonoBehaviour
+    {
+        public void DealDamage()
+        {
+            //Player Health -= damage
+        }
+    }
+    public class Chicken : Enemy
+    {
+
     }
 }
