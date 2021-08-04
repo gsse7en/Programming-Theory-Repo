3482a4114474956beb70be72f7f8007e315177aa
GsSe7en <latyshev.fedor@gmail.com>
2021-08-04 15:54:53 +0300

    add player name

--- Programming Theory Project/Assets/Scripts/SpawnManager.cs
+++ Programming Theory Project/Assets/Scripts/SpawnManager.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class SpawnManager : MonoBehaviour
 {
+    public TextMeshProUGUI playerName;
     public GameObject[] enemyTypes;
     public float spawnBound = 8f;
     public float startDelay = 1f;
@@ -12,6 +14,7 @@ public class SpawnManager : MonoBehaviour
     void Start()
     {
         InvokeRepeating("SpawnRandomEnemy", startDelay, spawnInterval);
+        playerName.text = GameManager.Instance.inputName;
     }
 
     // Update is called once per frame
